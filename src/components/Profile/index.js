aa15dc884f3293beae0149c94bcb0c4383704f91
yusuf <demir.yf@gmail.com>
2025-07-22 11:17:22 +0200

    update:presentation

--- src/components/Profile/index.js
+++ src/components/Profile/index.js
@@ -2,7 +2,7 @@ import React from "react";
 import styles from "./styles.module.css";
 
 const presentation = {
-  text: "Nouvellement développeur Web et anciennement Data Analyst. Je suis un accro des algorithmes, une partie de mon temps libre est consacrée sur CodinGame. J'ai fait des études en statistiques et après l’obtention de mon master, j'ai travaillé pendant six ans dans le domaine de la data. Je me suis rendu compte que finalement, ce qui m'intéressait le plus dans mon précédent métier, c'était toute la partie programmation et les algorithmes. A la suite de la liquidation de l'entreprise où je travaillais, j'ai saisi l'opportunité de suivre une formation chez O’clock afin de me reconvertir en tant que développeur. Je suis un développeur passionné qui réalisera tous vos projets avec enthousiasme !",
+  text: "Développeur Fullstack et intégrateur ESB, je dispose d’une double compétence en développement web et en analyse de données. Actuellement en reconversion, je recherche un poste plus concret, orienté technique, terrain ou maintenance. Curieux, rigoureux et passionné par les environnements techniques, j’aime résoudre les problèmes de façon efficace.",
 };
 
 export default function Profile() {
